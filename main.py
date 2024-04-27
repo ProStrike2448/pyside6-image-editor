205c893b626f01374b1f3faf57fca00dae78cbf6
ProStrike2448 <ostapenko.andrey03@gmail.com>
2024-04-27 11:45:28 +0300

    Add function for choosing/listing images

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
 import sys
+from typing import Sequence
 
 from PySide6.QtWidgets import (
     QApplication,
+    QFileDialog,
     QHBoxLayout,
     QLabel,
     QListWidget,
@@ -47,6 +50,35 @@ hl_main.addLayout(vl_files, stretch=1)
 hl_main.addLayout(vl_image, stretch=3)
 
 window.setLayout(hl_main)
+
+cwd = ""
+
+
+def filter_images(filenames: Sequence[str]):
+    result: list[str] = []
+
+    for filename in filenames:
+        if os.path.splitext(filename)[1] in (".jpg", ".jpeg", ".png", ".gif", ".bmp"):
+            result.append(filename)
+
+    return result
+
+
+def choose_cwd():
+    """Current working directory"""
+
+    global cwd
+    cwd = QFileDialog.getExistingDirectory()
+
+
+def show_images_names():
+    choose_cwd()
+    lw_files.clear()
+    lw_files.addItems(filter_images(os.listdir(cwd)))
+
+
+btn_dir.clicked.connect(show_images_names)
+
 
 window.show()
 app.exec()
